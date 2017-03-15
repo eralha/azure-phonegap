52a9ad4e145554c6f2962c86b85ef9ffde010d92
Emanuel <emaralha@yahoo.com>
2017-03-15 11:29:58 +0000

    add ADAL

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,3 +1,31 @@
 var ControllersModule = angular.module('starter.controllers', [])
 
-ControllersModule.controller('DashCtrl', function($scope) {});
+ControllersModule.controller('DashCtrl', function($scope) {
+
+	// Shows user authentication dialog if required
+	function authenticate(authCompletedCallback, errorCallback) {
+	  var authContext = new Microsoft.ADAL.AuthenticationContext(authority);
+	  authContext.tokenCache.readItems().then(function (items) {
+	    if (items.length > 0) {
+	        authority = items[0].authority;
+	        authContext = new Microsoft.ADAL.AuthenticationContext(authority);
+	    }
+	    // Attempt to authorize user silently
+	    authContext.acquireTokenSilentAsync(resourceUri, clientId)
+	    .then(authCompletedCallback, function () {
+	        // We require user credentials so triggers authentication dialog
+	        authContext.acquireTokenAsync(resourceUri, clientId, redirectUri)
+	        .then(authCompletedCallback, errorCallback);
+	    });
+	  });
+	};
+
+	authenticate(function(authResponse) {
+	  console.log("Token acquired: " + authResponse.accessToken);
+	  console.log("Token will expire on: " + authResponse.expiresOn);
+	}, function(err) {
+	  console.log("Failed to authenticate: " + err);
+	});
+
+
+});
